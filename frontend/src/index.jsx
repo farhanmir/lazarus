d6d753e2b0ced2828455d7150e4ff35445e48e78
Dimural Murat <dimural722@gmail.com>
2026-04-19 00:04:34 -0400

    fix

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import Landing from './pages/Landing'
 import AgentTrace from './pages/AgentTrace'
@@ -10,7 +10,7 @@ function Root() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/dashboard?tab=rescue" replace />} />
+        <Route path="/" element={<Landing />} />
         <Route path="/welcome" element={<Landing />} />
         <Route path="/dashboard" element={<App />} />
         <Route path="/agents/:runId" element={<AgentTrace />} />
